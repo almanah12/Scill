04f24a84c33c2b56e2f8db15cadff315d8e2569e
Almas Murza <almas.murza@mail.ru>
2020-11-02 16:58:51 +0600

    entered information about my neigbors(name,heigh), calculated overall us heigh

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -4,17 +4,22 @@
 # Создайте списки:
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-my_family = []
+my_family = ['abay', 'ora', 'almas']
 
 
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
     # ['имя', рост],
-    [],
+    ['abay', 178],
+    ['ora', 180],
+    ['almas', 173],
 ]
 
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
-
+print('Heigh abay - '+ str(my_family_height[0][1])+'cm')
+print('Heigh ora - '+ str(my_family_height[1][1])+'cm')
+print('Heigh almas - '+ str(my_family_height[2][1])+'cm')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
-#   Общий рост моей семьи - ХХ см
+#   The overall growth of my family - ХХ см
+print('The overall growth of my family - '+ str(my_family_height[0][1]+my_family_height[1][1]+my_family_height[1][1])+'cm')
