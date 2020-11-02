b5c983e2b89dd1e39e56104fcc416b9b1bde6f78
Almas Murza <almas.murza@mail.ru>
2020-11-02 15:44:01 +0600

    Вычислил расст между городами используя СЛОВАРЬ коорд городов

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -12,10 +12,14 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+distances = {'Moscow to London': (((sites['Moscow'][0]-sites['London'][0])**2+(sites['Moscow'][1]-sites['London'][1])**2)**1/2),
+             'Moscow to Paris': (((550-480)**2+(370-480)**2)**1/2),
+             'London to Paris': (((510-480)**2+(510-480)**2)**1/2),
+             }
 
 # TODO здесь заполнение словаря
-
+print(sites['Moscow'][0])
+print(sites)
 print(distances)
 
 
