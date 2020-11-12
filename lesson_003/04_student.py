1c41db3e6dccc9cf7fc4fd3f977568ee1871edd6
Almas Murza <almas.murza@mail.ru>
2020-11-12 14:28:10 +0600

    I used cycle for, to calculate how much money need to ask parents

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -10,5 +10,14 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
-
+summa_expenses = 0
+i = 0
 # TODO здесь ваш код
+for i in range(10):
+    if i == 0:
+        summa_expenses += expenses
+    else:
+        summa_expenses += expenses + expenses * 0.03
+
+sum_ask_parents = summa_expenses - educational_grant * 10
+print('the amount to ask the parents:', sum_ask_parents)
