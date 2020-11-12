a5d2eed01a2b9ae8720b5df8940bdb4326e45087
Almas Murza <almas.murza@mail.ru>
2020-11-12 13:09:13 +0600

    I used cycle if/else

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -19,13 +19,13 @@ envelop_x, envelop_y = 10, 7
 paper_x, paper_y = 11, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
-one_side = envelop_x > paper_x or envelop_x > paper_y
-another_side = envelop_y > paper_x or envelop_y > paper_y
-
-if one_side and another_side:
-    print('Does the paper fit in the envelope: Yes')
-else:
-    print('Does the paper fit in the envelope: no')
+# one_side = envelop_x > paper_x or envelop_x > paper_y
+# another_side = envelop_y > paper_x or envelop_y > paper_y
+#
+# if one_side and another_side:
+#     print('Does the paper fit in the envelope: Yes')
+# else:
+#     print('Does the paper fit in the envelope: no')
 
 #  задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -33,8 +33,8 @@ else:
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+#brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -46,12 +46,27 @@ else:
 # brick_x, brick_y, brick_z = 6, 5, 3
 # brick_x, brick_y, brick_z = 5, 6, 3
 # brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
+#brick_x, brick_y, brick_z = 11, 3, 6
 # brick_x, brick_y, brick_z = 11, 6, 3
 # brick_x, brick_y, brick_z = 6, 11, 3
 # brick_x, brick_y, brick_z = 6, 3, 11
 # brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
 # TODO здесь ваш код
+
+if hole_x > brick_x and hole_y > brick_y:
+    print('Does the paper fit in the envelope: Yes')
+elif hole_x > brick_y and hole_y > brick_x:
+        print('Does the paper fit in the envelope: Yes')
+elif hole_x > brick_x and hole_y > brick_z:
+        print('Does the paper fit in the envelope: Yes')
+elif hole_x > brick_z and hole_y > brick_x:
+        print('Does the paper fit in the envelope: Yes')
+elif hole_x > brick_y and hole_y > brick_z:
+        print('Does the paper fit in the envelope: Yes')
+elif hole_x > brick_z and hole_y > brick_y:
+        print('Does the paper fit in the envelope: Yes')
+else:
+    print('Does the paper fit in the envelope: no')
