97bdd6af017e8d5ad3ca54dd66c4f432620a96ce
Almas Murza <almas.murza@mail.ru>
2020-11-16 14:08:13 +0600

    I used cycle nested for loops and ciycle if/else.

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -6,12 +6,11 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
 
 for k in range(12):
     for i in range(7):
         if k%2 == 0:
-            simple_draw.rectangle(simple_draw.get_point(0 + i*100, k*50), simple_draw.get_point(100 + i*100, 50 + k*50), width=1)
+            simple_draw.rectangle(simple_draw.get_point(i*100, k*50), simple_draw.get_point(100 + i*100, 50 + k*50), width=1)
         elif k%2 != 0:
             simple_draw.rectangle(simple_draw.get_point(-50 + i*100, k*50), simple_draw.get_point(50 + i*100, 50 + k*50), width=1)
 
