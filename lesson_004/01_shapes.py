96eee3a7aade97624b8c461fee5d7d406a051e3f
Almas Murza <almas.murza@mail.ru>
2020-11-17 15:00:58 +0600

    forget the commit

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -2,82 +2,83 @@
 
 import simple_draw as sd
 sd.resolution = [800, 800]
-sd.s
-
-def triangle(angles_point, angle=0, length=100):
-    v1 = sd.get_vector(start_point=angles_point, angle=angle, length=length)
-    v1.draw()
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=120 + angle, length=length)
-    v2.draw()
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=240 + angle, length=length)
-    v3.draw()
 
 
-point = sd.get_point(50, 50)
-triangle(angles_point=point, angle=0, length=200)
+# def triangle(angles_point, angle=0, length=100):
+#     v1 = sd.get_vector(start_point=angles_point, angle=angle, length=length)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=120 + angle, length=length)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=240 + angle, length=length)
+#     v3.draw()
+#
+#
+# point = sd.get_point(50, 50)
+# triangle(angles_point=point, angle=0, length=200)
+#
+#
+# def sguare(start_poin, angle=0, length=100):
+#     v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=90 + angle, length=length)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=180 + angle, length=length)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=270 + angle, length=length)
+#     v4.draw()
+#
+#
+# point_square = sd.get_point(500, 100)
+# sguare(start_poin=point_square, angle=0, length=100)
+#
+#
+# def pentagon(start_poin, angle=0, length=100):
+#     v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=72 + angle, length=length)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=144 + angle, length=length)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=216 + angle, length=length)
+#     v4.draw()
+#
+#     v5 = sd.get_vector(start_point=v4.end_point, angle=288 + angle, length=length)
+#     v5.draw()
+# point_pentagon = sd.get_point(50, 500)
+# pentagon(start_poin=point_pentagon, angle=0)
+#
+#
+# def hexagon(start_poin, angle=0, length=100):
+#     v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=60 + angle, length=length)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=120 + angle, length=length)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=180 + angle, length=length)
+#     v4.draw()
+#
+#     v5 = sd.get_vector(start_point=v4.end_point, angle=240 + angle, length=length)
+#     v5.draw()
+#
+#     v6 = sd.get_vector(start_point=v5.end_point, angle=301 + angle, length=length)
+#     v6.draw()
+#
+#
+# point_hexagon = sd.get_point(500, 500)
+# hexagon(start_poin=point_hexagon, angle=0, length=100)
 
-
-def sguare(start_poin, angle=0, length=100):
-    v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
-    v1.draw()
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=90 + angle, length=length)
-    v2.draw()
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=180 + angle, length=length)
-    v3.draw()
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=270 + angle, length=length)
-    v4.draw()
-
-
-point_square = sd.get_point(500, 100)
-sguare(start_poin=point_square, angle=0, length=100)
-
-
-def pentagon(start_poin, angle=0, length=100):
-    v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
-    v1.draw()
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=72 + angle, length=length)
-    v2.draw()
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=144 + angle, length=length)
-    v3.draw()
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=216 + angle, length=length)
-    v4.draw()
-
-    v5 = sd.get_vector(start_point=v4.end_point, angle=288 + angle, length=length)
-    v5.draw()
-point_pentagon = sd.get_point(50, 500)
-pentagon(start_poin=point_pentagon, angle=0)
-
-
-def hexagon(start_poin, angle=0, length=100):
-    v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
-    v1.draw()
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=60 + angle, length=length)
-    v2.draw()
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=120 + angle, length=length)
-    v3.draw()
-
-    v4 = sd.get_vector(start_point=v3.end_point, angle=180 + angle, length=length)
-    v4.draw()
-
-    v5 = sd.get_vector(start_point=v4.end_point, angle=240 + angle, length=length)
-    v5.draw()
-
-    v6 = sd.get_vector(start_point=v5.end_point, angle=301 + angle, length=length)
-    v6.draw()
-
-
-point_hexagon = sd.get_point(500, 500)
-hexagon(start_poin=point_hexagon, angle=0, length=100)
 
 # Часть 1.
 # Написать функции рисования равносторонних геометрических фигур:
@@ -105,6 +106,24 @@ hexagon(start_poin=point_hexagon, angle=0, length=100)
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
 # TODO здесь ваш код
+
+def common_angles(start_poin, angle=0, length=100, side = 1):
+    for i in range(side):
+        v2 = sd.get_vector(start_point=start_poin, angle= 360*(i/side) + angle, length=length)
+        v2 = v2.end_point
+        v2.draw()
+
+
+point_common = sd.get_point(100, 100)
+common_angles(start_poin=point_common, angle=0, side=3)
+
+
+# def pentagon(start_poin, angle=0, length=100):
+#     v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=72 + angle, length=length)
+#     v2.draw()
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
