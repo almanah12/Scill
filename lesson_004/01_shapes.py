c4a3941f9a2e138f04f8400747aab08a0b09b105
Almas Murza <almas.murza@mail.ru>
2020-11-17 13:43:42 +0600

    add one vector to have 5 vectors

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -31,6 +31,21 @@ def sguare(start_poin, angle=0, length=100):
 point_square = sd.get_point(500, 100)
 sguare(start_poin=point_square, angle=0, length=100)
 
+def pentagon(start_poin, angle=0, length=100):
+    v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=90 + angle, length=length)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=180 + angle, length=length)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=270 + angle, length=length)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v3.end_point, angle=270 + angle, length=length)
+    v5.draw()
 # Часть 1.
 # Написать функции рисования равносторонних геометрических фигур:
 # - треугольника
