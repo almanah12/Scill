304540273e08be3093da2af70fa036a719cb8b2d
Almas Murza <almas.murza@mail.ru>
2020-11-17 13:31:46 +0600

    wrote a triangle function call, and fixed the starting points of the second and third vectors

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -7,14 +7,14 @@ def triangle(start_poin, angle=0, length=100):
     v1 = sd.get_vector(start_point=start_poin, angle=angle, length=length)
     v1.draw()
 
-    v2 = sd.get_vector(start_point=start_poin, angle=120 + angle, length=length)
+    v2 = sd.get_vector(start_point=v1.end_point, angle=120 + angle, length=length)
     v2.draw()
 
-    v3 = sd.get_vector(start_point=start_poin, angle=240 + angle, length=length)
+    v3 = sd.get_vector(start_point=v2.end_point, angle=240 + angle, length=length)
     v3.draw()
 
 point = sd.get_point(50, 50)
-triangle(start_poin=point, an)
+triangle(start_poin=point, angle=0, length=200)
 
 # Часть 1.
 # Написать функции рисования равносторонних геометрических фигур:
