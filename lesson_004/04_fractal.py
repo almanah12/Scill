611ec93f0456322bb372db4b2f9d3591e63ebe3d
Almas Murza <almas.murza@mail.ru>
2020-11-18 15:50:18 +0600

    wrote code that drawing picture tree, in the previous code 3 vectors was a mistake, the error found when using random data

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -4,30 +4,52 @@ import simple_draw as sd
 sd.resolution = [800, 800]
 
 
-def draw_bunches(start_point, angle1, angle2, angle3, length,):
+# def draw_bunches(start_point, angle1, angle2, angle3, length,):
+#     if length < 5:
+#         return
+#     v1 = sd.get_vector(start_point=start_point, angle=angle1, length=length,)
+#     v1.draw()
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle2, length=length*.75,)
+#     v2.draw()
+#     v3 = sd.get_vector(start_point=v1.end_point, angle=angle3, length=length*.75,)
+#     v3.draw()
+#     next_point = v1.end_point
+#     next_angle1 = angle1 - sd.random_number(18, 42)
+#     next_angle2 = angle2 - sd.random_number(18, 42)
+#     next_angle3 = angle3 - sd.random_number(18, 42)
+#     next_length = length*.75
+#     draw_bunches(start_point=next_point, angle1=next_angle1, angle2=next_angle2, angle3=next_angle3, length=next_length,)
+#     next_angle1 = angle1 + sd.random_number(18, 42)
+#     next_angle2 = angle2 + sd.random_number(18, 42)
+#     next_angle3 = angle3 + sd.random_number(18, 42)
+#     next_length = length*.75
+#     draw_bunches(start_point=next_point, angle1=next_angle1, angle2=next_angle2, angle3=next_angle3, length=next_length,)
+#
+#
+# root_point = sd.get_point(300, 30)
+# draw_bunches(start_point=root_point, angle1=90, angle2=120, angle3=60, length=100, )
+
+def draw_bunches(start_point, angle1, length,):
     if length < 5:
         return
     v1 = sd.get_vector(start_point=start_point, angle=angle1, length=length,)
     v1.draw()
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle2, length=length*.75,)
-    v2.draw()
-    v3 = sd.get_vector(start_point=v1.end_point, angle=angle3, length=length*.75,)
-    v3.draw()
+
     next_point = v1.end_point
     next_angle1 = angle1 - 30
-    next_angle2 = angle2 - 30
-    next_angle3 = angle3 - 30
     next_length = length*.75
-    draw_bunches(start_point=next_point, angle1=next_angle1, angle2=next_angle2, angle3=next_angle3, length=next_length,)
+    draw_bunches(start_point=next_point, angle1=next_angle1, length=next_length,)
+    next_point = v1.end_point
     next_angle1 = angle1 + 30
-    next_angle2 = angle2 + 30
-    next_angle3 = angle3 + 30
     next_length = length*.75
-    draw_bunches(start_point=next_point, angle1=next_angle1, angle2=next_angle2, angle3=next_angle3, length=next_length,)
+    draw_bunches(start_point=next_point, angle1=next_angle1, length=next_length,)
+
 
 
 root_point = sd.get_point(300, 30)
-draw_bunches(start_point=root_point, angle1=90, angle2=120, angle3=60, length=100, )
+draw_bunches(start_point=root_point, angle1=90, length=100, )
+
+
 
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
@@ -61,7 +83,7 @@ draw_bunches(start_point=root_point, angle1=90, angle2=120, angle3=60, length=10
 # Возможный результат решения см lesson_004/results/exercise_04_fractal_02.jpg
 
 # Пригодятся функции
-sd.random_number()
+#sd.random_number()
 
 sd.pause()
 
--- test.py
+++ test.py
@@ -1,26 +1,9 @@
+# import simple_draw as sd
+# sd.resolution = [800, 800]
+#
+# aser = sd.random_number(6.5, float(8))
+# print(aser)
 
-import simple_draw as sd
-
-sd.resolution = (800, 600)
-
-
-def branch(point, angle, length):
-    if length < 1:
-        return
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=2)
-    v1.draw()
-
-    next_point = v1.end_point
-    next_angle = v1.angle - 30
-    next_length = v1.length * .75
-
-    branch(point=next_point, angle=next_angle, length=next_length)
-
-
-point_0 = sd.get_point(200,200)
-branch(point=point_0, angle=90, length=50)
-branch(point=point_0, angle=90, length=100)
-branch(point=point_0, angle=90, length=200)
-
-
-sd.pause()
+import random
+x = random.uniform(10.5, 100.5)
+print("Random float number between range 10.5 to 100.5 ", x)
