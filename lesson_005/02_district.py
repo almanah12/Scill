0539c94dfd4cc991ad3645555c4fe84ddc5ed0cb
alash <almas.mrza@mail.ru>
2020-11-19 18:14:00 +0600

    import all of the direct, and i want display all of the direct residents

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,8 +5,15 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from central_street.house1 import room1 as cs_h1_r1
+from central_street.house1 import room2 as cs_h1_r2
+from central_street.house2 import room1 as cs_h2_r1
+from central_street.house2 import room2 as cs_h2_r2
+from soviet_street.house1 import room1 as ss_h1_r1
+from soviet_street.house1 import room2 as ss_h1_r2
+from soviet_street.house2 import room1 as ss_h2_r1
+from soviet_street.house2 import room2 as ss_h2_r2
 
-
-
+print('In live the area:', ','.cs_)
+cs_h1_r1.folks
 
