8b050a715d615751f5e4164ad20156757d1d97a3
alash <almas.mrza@mail.ru>
2020-11-19 19:25:52 +0600

    display district residents in one list, no new newline, using cyycle if/elif and operaator "end"

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -14,6 +14,12 @@ from soviet_street.house1 import room2 as ss_h1_r2
 from soviet_street.house2 import room1 as ss_h2_r1
 from soviet_street.house2 import room2 as ss_h2_r2
 
-print('In live the area:', ','.cs_)
-cs_h1_r1.folks
+houses = [cs_h1_r1, cs_h1_r2, cs_h2_r1, cs_h2_r2, ss_h1_r1, ss_h1_r2, ss_h2_r1, ss_h2_r2]
+
+print('Live in the district:')
+for i in range(len(houses)):
+    if i != len(houses) - 1 and bool(houses[i].folks):
+        print(','.join(houses[i].folks), end=",")
+    elif i == len(houses) - 1 and bool(houses[i].folks):
+        print(','.join(houses[i].folks))
 
