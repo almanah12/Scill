fa2b4f80c763cbd050be26b57d0891758ee9ab30
Almas Murza <almas.murza@mail.ru>
2020-11-20 15:27:51 +0600

    made it comfortable

--- lesson_005/Drawing/set_drawing/mod_drawing.py
+++ lesson_005/Drawing/set_drawing/mod_drawing.py
@@ -3,20 +3,23 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 simple_draw.resolution = [1100, 700]
+rainbow_colors = (simple_draw.COLOR_RED, simple_draw.COLOR_ORANGE, simple_draw.COLOR_YELLOW, simple_draw.COLOR_GREEN,
+                  simple_draw.COLOR_CYAN, simple_draw.COLOR_BLUE, simple_draw.COLOR_PURPLE)
 
 
 def add_wall():
-    for k in range(12):
-        for i in range(7):
+    left_x, left_y = 200, 50
+    length_brick, hight_brick = 60, 30
+    right_x = left_x + length_brick
+    right_y = left_y + hight_brick
+
+    for k in range(8):
+        for i in range(5):
             if k%2 == 0:
-                simple_draw.rectangle(simple_draw.get_point(i*100, k*50), simple_draw.get_point(100 + i*100, 50 + k*50), width=1)
-            elif k%2 != 0:
-                simple_draw.rectangle(simple_draw.get_point(-50 + i*100, k*50), simple_draw.get_point(50 + i*100, 50 + k*50), width=1)
+                simple_draw.rectangle(simple_draw.get_point(left_x + i*length_brick, left_y + k*hight_brick), simple_draw.get_point(right_x + i*length_brick, right_y + k*hight_brick), width=1)
+            # elif k%2 != 0:
+            #     simple_draw.rectangle(simple_draw.get_point(-50 + i*100, k*50), simple_draw.get_point(50 + i*100, 50 + k*50), width=1)
     simple_draw.pause()
-
-
-rainbow_colors = (simple_draw.COLOR_RED, simple_draw.COLOR_ORANGE, simple_draw.COLOR_YELLOW, simple_draw.COLOR_GREEN,
-                  simple_draw.COLOR_CYAN, simple_draw.COLOR_BLUE, simple_draw.COLOR_PURPLE)
 
 
 def rainbow(point, step):
