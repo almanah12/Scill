d96167fe8851d3725ba7a22699cbcdb2d6b52226
alash <almas.mrza@mail.ru>
2020-11-21 13:35:46 +0600

    add color in vector,

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,7 +3,12 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: Вc ...
 
-import room_1
-for i in range(len(room_1.folks)):
-    print('Room name "room_1" live:', room_1.folks[i])
+# import room_1
+# for i in range(len(room_1.folks)):
+#     print('Room name "room_1" live:', room_1.folks[i])
+#
 
+
+import module_1
+print(module_1.variable_1)
+module_1.function1()
--- lesson_005/Drawing/mod_drawing.py
+++ lesson_005/Drawing/mod_drawing.py
@@ -8,7 +8,9 @@ rainbow_colors = (simple_draw.COLOR_RED, simple_draw.COLOR_ORANGE, simple_draw.C
 
 
 def add_wall():
-    #simple_draw._to_screen_rect(simple_draw.get_point(200, 50), simple_draw.get_point(500, 290))
+    """
+    wall
+    """
     left_x, left_y = 200, 50
     length_brick, hight_brick = 60, 30
     right_x = left_x + length_brick
@@ -17,18 +19,28 @@ def add_wall():
     column = 8
     for k in range(column):
         for i in range(row):
-            if k%2 == 0:
-                simple_draw.rectangle(simple_draw.get_point(left_x + i*length_brick, left_y + k*hight_brick), simple_draw.get_point(right_x + i*length_brick, right_y + k*hight_brick), width=1)
-            elif k%2 != 0:
+            if k % 2 == 0:
+                simple_draw.rectangle(simple_draw.get_point(left_x + i * length_brick, left_y + k * hight_brick),
+                                      simple_draw.get_point(right_x + i * length_brick, right_y + k * hight_brick),
+                                      width=1)
+            elif k % 2 != 0:
                 if i == 0 or i == row - 1:
-                    simple_draw.rectangle(simple_draw.get_point(left_x + i*length_brick, left_y + k*hight_brick), simple_draw.get_point(right_x + i*length_brick, right_y + k*hight_brick), width=1)
+                    simple_draw.rectangle(simple_draw.get_point(left_x + i * length_brick, left_y + k * hight_brick),
+                                          simple_draw.get_point(right_x, right_y + k * hight_brick),
+                                          simple_draw.COLOR_DARK_ORANGE, width=1)
                 else:
-                    simple_draw.rectangle(simple_draw.get_point((left_x - length_brick/2) + i*length_brick, left_y + k*hight_brick), simple_draw.get_point((left_x + length_brick/2) + i*length_brick, right_y + k*hight_brick), width=1)
+                    simple_draw.rectangle(
+                        simple_draw.get_point((left_x - length_brick / 2) + i * length_brick, left_y + k * hight_brick),
+                        simple_draw.get_point((left_x + length_brick / 2) + i * length_brick,
+                                              right_y + k * hight_brick), width=1)
 
     simple_draw.pause()
 
 
 def rainbow(point, step):
+    """
+    rainbow
+    """
     radius = 1200
     for i in range(7):
         radius += step
@@ -38,11 +50,6 @@ def rainbow(point, step):
 
 def call_rainbow():
     rainbow(point=simple_draw.get_point(100, -250), step=4)
-    simple_draw.finish_drawing()
-
-
-def call_test():
-    simple_draw.rectangle(simple_draw.get_point(50, 50), simple_draw.get_point(120, 100))
     simple_draw.finish_drawing()
 
 
