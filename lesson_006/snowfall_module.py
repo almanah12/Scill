10e3598c556735ab239f3fd8f7b4b22acd7412a8
alash <almas.mrza@mail.ru>
2020-11-29 21:55:05 +0600

    наконецто закончил

--- lesson_006/snowfall_module.py
+++ lesson_006/snowfall_module.py
@@ -5,20 +5,18 @@ import random
 
 # На основе кода из lesson_004/05_snowfall.py
 # сделать модуль snowfall.py в котором реализовать следующие функции
-#  создать_снежинки(N) - создает N снежинок
-#  нарисовать_снежинки_цветом(color) - отрисовывает все снежинки цветом color
-#  сдвинуть_снежинки() - сдвигает снежинки на один шаг
-#  номера_достигших_низа_экрана() - выдает список номеров снежинок, которые вышли за границу экрана
-#  удалить_снежинки(номера) - удаляет снежинки с номерами из списка
-#
+
+
 # В текущем модуле реализовать главный цикл падения снежинок,
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 
+
 _point_x = []
 _point_y = []
-
 _count = []
 
+
+#  создать_снежинки(N) - создает N снежинок
 def create_snow(N):
     # глобальное переменное для доступа на всех функциях
     global _point_x, _point_y
@@ -27,40 +25,41 @@ def create_snow(N):
         _point_x.append(sd.random_number(50, 1000))
 
     for i in range(N):
-        _point_y.append(sd.random_number(550, 600))
+        _point_y.append(sd.random_number(630, 650))
 
 
+#  нарисовать_снежинки_цветом(color) - отрисовывает все снежинки цветом color
 def draw_snow_color(color):
     sd.start_drawing()
     for i in range(len(_point_x)):
         center = sd.get_point(_point_x[i], _point_y[i])
         #  - отрисовка снежинки с цветом color
         sd.snowflake(center=center, length=50, color=color)
-
     sd.finish_drawing()
 
 
+#  сдвинуть_снежинки() - сдвигает снежинки на один шаг
 def move_snowflakes():
-    # - сдвинуть снежинку
     for i in range(len(_point_x)):
-        _point_y[i] = _point_y[i] - sd.random_number(10, 20)
+        _point_y[i] = _point_y[i] - sd.random_number(10, 100)  # верт.скорость падения снега
 
 
+#  номера_достигших_низа_экрана() - выдает список номеров снежинок, которые вышли за границу экрана
 def screen_numbers_reached_down():
     global _count
+    _count = []
     for i in range(len(_point_x)):
-        if _point_y[i] < sd.random_number(40, 60):
+        if _point_y[i] < sd.random_number(80, 101):
             _count.append(i)
-    print(_count)
     return _count
 
 
+#  удалить_снежинки(номера) - удаляет снежинки с номерами из списка
 def delete_snowflakes(count):
-    for i in range(len(count)):
-        center = sd.get_point(_point_x[count[i]], _point_y[count[i]])
-        sd.start_drawing()
-        sd.snowflake(center=center, length=50, color=sd.background_color)
-        sd.finish_drawing()
+    center = sd.get_point(_point_x[count], _point_y[count])
+    sd.start_drawing()
+    sd.snowflake(center=center, length=50, color=sd.background_color)
+    sd.finish_drawing()
+    del _point_x[count]
+    del _point_y[count]
 
-        del _point_x[count[i]]
-        del _point_y[count[i]]
