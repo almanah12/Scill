55de7842801a8323b18be5a127c703ee51134e57
alash <almas.mrza@mail.ru>
2020-12-01 13:40:54 +0600

    solved 7 module, 2 tack. Using operator isinstance() for addition class

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -4,22 +4,83 @@
 # Реализовать следующие элементы: Вода, Воздух, Огонь, Земля, Шторм, Пар, Грязь, Молния, Пыль, Лава.
 # Каждый элемент организовать как отдельный класс.
 # Таблица преобразований:
+# Сложение элементов реализовывать через __add__
+# Если результат не определен - то возвращать None
+# Вывод элемента на консоль реализовывать через __str__
+
 #   Вода + Воздух = Шторм
 #   Вода + Огонь = Пар
 #   Вода + Земля = Грязь
 #   Воздух + Огонь = Молния
 #   Воздух + Земля = Пыль
 #   Огонь + Земля = Лава
-
-# Сложение элементов реализовывать через __add__
-# Если результат не определен - то возвращать None
-# Вывод элемента на консоль реализовывать через __str__
-#
 # Примеры преобразований:
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+
+class Water:
+    def __str__(self):
+        return 'Water'
+
+    def __add__(self, other):
+        if isinstance(self, Water) and isinstance(other, Air):
+            return Storm()
+
+
+class Air:
+    def __str__(self):
+        return 'Air'
+
+    def __add__(self, other):
+        if Water() + Air():
+            return Storm()
+
+
+class Fire:
+    def __str__(self):
+        return 'Fire'
+
+    def __add__(self, other):
+        new_object = Air()
+
+
+class Earth:
+    def __str__(self):
+        return 'Earth'
+
+    def __add__(self, other):
+        new_object = Air()
+
+
+class Dust:
+    pass
+
+
+class Lava:
+    pass
+
+
+class Dirt:
+    def __str__(self):
+        return 'Dirt'
+
+
+class Lightning:
+    pass
+
+
+class Steam:
+    pass
+
+
+class Storm:
+    def __str__(self):
+        return 'Storm'
+
+
+print(Water(), '+', Air(), '=', Air() + Water())
+print(Water(), '+', Earth(), '=', Water()+Earth())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
